ddb458bc790ab04da832f7b01e9529f3d6757ce4
TimSadko <tim.sadko@gmail.com>
2024-10-27 23:50:49 +0100

    Added phone version

---
+++ scripts/scripts.js
@@ -0,0 +1,66 @@
+function menu_side_bar_open() {
+    document.getElementById("menu_side").style.display = "inline-flex";
+  }
+  
+  function menu_side_bar_close() {
+    document.getElementById("menu_side").style.display = "none";
+  }
+
+  function go_to_login_page() {
+      window.open("login.html",'_self');
+  }
+
+  function go_to_home_page() {
+      window.open("index.html",'_self');
+  }
+
+  function go_to_register_page() {
+      window.open("register.html",'_self');
+  }
+
+  function go_to_paleontology_page() {
+      window.open("side_paleontology.html",'_self');
+  }
+
+  function go_to_art_page() {
+    window.open("side_art.html",'_self');
+ }
+
+ function go_to_botany_page() {
+    window.open("side_botany.html",'_self');
+ }
+
+ function go_to_history_page() {
+    window.open("side_history.html",'_self');
+ }
+
+
+
+function phone_go_to_home_page() {
+    window.open("phone_index.html",'_self');
+}
+
+function phone_go_to_register_page() {
+    window.open("phone_register.html",'_self');
+}
+
+function phone_go_to_login_page() {
+    window.open("phone_login.html",'_self');
+}
+
+function phone_go_to_paleontology_page() {
+    window.open("phone_side_paleontology.html",'_self');
+}
+
+function phone_go_to_art_page() {
+  window.open("phone_side_art.html",'_self');
+}
+
+function phone_go_to_botany_page() {
+  window.open("phone_side_botany.html",'_self');
+}
+
+function phone_go_to_history_page() {
+  window.open("phone_side_history.html",'_self');
+}
+  
